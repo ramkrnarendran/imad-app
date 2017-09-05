1af015e5c6fa3e44c79d21ecd44ea5e9d49b5665
ramkrnarendran <30928487+ramkrnarendran@users.noreply.github.com>
2017-09-05 16:02:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-two', function(req,res){
-     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+     res.sendFile(path.join(__dirname,  'article-two.html'));
 }
 );
 
