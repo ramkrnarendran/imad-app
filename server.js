d28057ab6554cf5fe11c68e37ecfb71d02a56f81
ramkrnarendran <30928487+ramkrnarendran@users.noreply.github.com>
2017-09-05 14:36:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article-one', function(req,res){
+    res.send('Article one requested and will be served here');
+}
+);
+
 app.get('/article-two', function(req,res){
     res.send('Article two requested and will be served here');
 }
