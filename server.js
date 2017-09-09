93d31512bd7d40754d29abce8f6c6932f45963c3
ramkrnarendran <30928487+ramkrnarendran@users.noreply.github.com>
2017-09-09 13:26:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/ui/article-two', function (req, res) {
 });
 app.get('/ui/article-three', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-  
+});
   
 app.get('/article-two', function(req,res){
     res.send('Article two requested and will be served here');
